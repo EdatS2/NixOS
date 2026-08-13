0ecf41bd4a2218b9941b5da0caea3a305aa2e9a3
Tade Salverda <tadesalverda@outlook.com>
2026-08-13 23:05:51 +0200

    Lanzaboote update and greetd fix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.3";
+      url = "github:nix-community/lanzaboote/v1.1.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-vfio.url = "github:j-brn/nixos-vfio";
--- lenovo/greetd.nix
+++ lenovo/greetd.nix
@@ -6,13 +6,7 @@
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = ''
-        			${pkgs.tuigreet}/bin/tuigreet \
-        			--time \
-        			--asterisks \
-        			--user-menu \
-        			--cmd Hyprland
-        		'';
+      default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --asterisks --user-menu --cmd Hyprland";
     };
   };
   environment.etc."greetd/environments".text = ''
